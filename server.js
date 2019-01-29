bf4fc62700f93f122623b499f86a2040cf084389
Avneesh Rai <avneesh.rai@jda.com>
2019-01-29 20:45:03 +0530

    list of project changes 2

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', (request, response) => {
   response.render('home.hbs', {
     pageTitile: 'Home page',
     // currentYear: new Date().getFullYear(),
-    welcomeMessage: 'Welcome to my website'
+    welcomeMessage: 'Welcome to my website Babu'
   });
 });
 
